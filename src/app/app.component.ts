063ce73a232d150be0b2dfb02f80b65cd59f5c2f
JamissonBarbosa <jamissondasilvafilho@gmail.com>
2024-08-28 12:37:01 -0300

    feat: create big-card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AngularMaterialModule } from './shared/angular-material/angular-material.module';
-import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AngularMaterialModule, MenuTitleComponent],
+  imports: [RouterOutlet, AngularMaterialModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
