4718bb74052c29742d29f082a8797106413d785b
JamissonBarbosa <jamissondasilvafilho@gmail.com>
2024-08-28 10:56:13 -0300

    feat: create menu-title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AngularMaterialModule } from './shared/angular-material/angular-material.module';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AngularMaterialModule, MenuTitleComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-blog';
